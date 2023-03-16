dfc4ab3cdbed200c421c12dd0532d77107710d1b
Кирилл Сысоев <k.sysoev@me.com>
2023-03-16 23:39:58 +0800

    Adds code generation for API

---
+++ generate.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+git clone https://github.com/binary-com/deriv-developers-portal.git
+pwd
+cd deriv-developers-portal/config/v3/
+pwd
+mkdir schema
+find . -type f | grep -v example | grep -v schema |  sed 'p; s/^\./.\/schema/; s/\/send/_request/; s/\/receive/_response/g' | xargs -n2 cp -f
+
+# Patch the schema for transaction response... Live without types so much easier
+sed  's/9999999999999999999/9223372036854775807/g' schema/transaction_response.json > schema/transaction_response_tmp.json
+mv schema/transaction_response_tmp.json schema/transaction_response.json
+gojsonschema -p deriv-api schema/*.json > ../../../shema.go 
+cd ../../../
+rm -rf deriv-developers-portal schema
